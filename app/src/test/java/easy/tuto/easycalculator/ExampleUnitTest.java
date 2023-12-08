974e162edd2f40be8459f61bab86030bd1d340c3
GluayTord <146327077+GluayTord@users.noreply.github.com>
2023-12-08 15:36:57 +0700

    Update ExampleUnitTest.java

--- app/src/test/java/easy/tuto/easycalculator/ExampleUnitTest.java
+++ app/src/test/java/easy/tuto/easycalculator/ExampleUnitTest.java
@@ -18,33 +18,4 @@ public class ExampleUnitTest {
         MathFunction math = new MathFunction();
         assertEquals("Undefined", math.power(0, 0));
     }
-    //Binary,Octal and Hex function test
-    /*@Test
-    public void testBinary(){
-        MathFunction math = new MathFunction();
-        assertEquals("101", math.decimalToBinary("5"));
-    }
-
-    @Test
-    public void testOctal(){
-        MathFunction math = new MathFunction();
-        assertEquals("64",math.decimalToOctal("52"));
-    }
-    @Test
-    public void testHex(){
-        MathFunction math = new MathFunction();
-        assertEquals("2E",math.decimalToHexadecimal("46"));
-    }
-    @Test
-    public void numberIsNegative(){
-        MathFunction math = new MathFunction();
-        assertEquals("Please enter a non-negative decimal number.",math.decimalToBinary("-50"));
-    }
-    @Test
-    public void numberIsNotInteger(){
-        MathFunction math = new MathFunction();
-        assertEquals("Please enter a decimal number that is an integer.",math.decimalToBinary("5.21"));
-    }
-    */
-
 }
